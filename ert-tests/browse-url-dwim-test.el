fd11d8482b74e46c895696cf9d629fde183a9297
Roland Walker <walker@pobox.com>
2012-10-10 19:49:36 -0400

    also require list-utils in test script

--- ert-tests/browse-url-dwim-test.el
+++ ert-tests/browse-url-dwim-test.el
@@ -3,14 +3,33 @@
 
 (when load-file-name
   (setq package-enable-at-startup nil)
-  (setq package-load-list '((string-utils t)))
+  (setq package-load-list '((list-utils t)
+                            (string-utils t)))
   (when (fboundp 'package-initialize)
     (package-initialize)))
 
+(require 'list-utils)
 (require 'string-utils)
 (require 'browse-url-dwim)
 
 ;;; this is a stub - no tests defined
+
+;;; browse-url-dwim-coerce-to-web-url (url &optional any-scheme add-scheme)
+
+;;; browse-url-dwim-add-prompt-default (prompt-string default-string &optional length-limit)
+
+;;; browse-url-dwim-context-url ()
+
+;;; browse-url-dwim-get-url (&optional always-prompt prompt-string fallback-default)
+
+;;; browse-url-dwim-make-search-prompt (search-url)
+
+;;; browse-url-dwim (url)
+
+;;; browse-url-dwim-search (&optional text search-url guess)
+
+;;; browse-url-dwim-guess (&optional text search-url)
+
 
 ;;
 ;; Emacs
