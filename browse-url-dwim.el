14d344c4a7251651d33693f9b702321fc871c4a5
Roland Walker <walker@pobox.com>
2012-10-16 17:21:46 -0400

    bump version

--- browse-url-dwim.el
+++ browse-url-dwim.el
@@ -5,8 +5,8 @@
 ;; Author: Roland Walker <walker@pobox.com>
 ;; Homepage: http://github.com/rolandwalker/browse-url-dwim
 ;; URL: http://raw.github.com/rolandwalker/browse-url-dwim/master/browse-url-dwim.el
-;; Version: 0.5.2
-;; Last-Updated: 14 Sep 2012
+;; Version: 0.6.0
+;; Last-Updated: 16 Oct 2012
 ;; EmacsWiki: BrowseUrlDwim
 ;; Keywords: hypermedia
 ;; Package-Requires: ((string-utils "0.0.3"))
@@ -187,7 +187,7 @@
 ;;;###autoload
 (defgroup browse-url-dwim nil
   "Context-sensitive external browse URL or Internet search."
-  :version "0.5.2"
+  :version "0.6.0"
   :link '(emacs-commentary-link "browse-url-dwim")
   :prefix "browse-url-dwim-"
   :group 'external
