59a5e64c003d24a1ec0e0deabfe518bf099b08c3
Roland Walker <walker@pobox.com>
2012-11-26 12:26:51 -0500

    improve compat of called-interactively-p macro including trunk (to be 24.3) and versions with only `interactive-p`

--- browse-url-dwim.el
+++ browse-url-dwim.el
@@ -383,9 +383,15 @@ associated prompt.")
 
 Optional KIND is as documented at `called-interactively-p'
 in GNU Emacs 24.1 or higher."
-  (if (eq 0 (cdr (subr-arity (symbol-function 'called-interactively-p))))
-      '(called-interactively-p)
-    `(called-interactively-p ,kind)))
+  (cond
+    ((not (fboundp 'called-interactively-p))
+     '(interactive-p))
+    ((condition-case nil
+         (progn (called-interactively-p 'any) t)
+       (error nil))
+     `(called-interactively-p ,kind))
+    (t
+     '(called-interactively-p))))
 
 ;;; compatibility functions
 
